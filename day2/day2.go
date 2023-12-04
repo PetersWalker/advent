eb5a70c50e8bc31c2e4ba11d953d5aece45d03e6
PetersWalker <petersinclairwalker@gmail.com>
2023-12-04 18:48:45 -0500

    day2 part 1

--- day2/day2.go
+++ day2/day2.go
@@ -1,5 +1,100 @@
-package day2
+package main
 
-func day2() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
+// The Elf would first like to know which games would have been
+// possible if the bag contained only 12 red cubes, 13 green cubes,
+// and 14 blue cubes?
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	count := processLines(scanner)
+	log.Println("total: ", count)
+}
+
+func processLines(s *bufio.Scanner) int {
+	possibleGames := 0
+
+	red := 12
+	green := 13
+	blue := 14
+
+	for s.Scan() {
+		possible := true
+		line := s.Text()
+
+		gameNumber := parseGameNumber(line)
+		colorCounts := parseColorValues(line)
+
+		for _, colorValues := range colorCounts {
+			if colorValues["red"] > red {
+				possible = false
+			}
+
+			if colorValues["green"] > green {
+				possible = false
+			}
+
+			if colorValues["blue"] > blue {
+				possible = false
+			}
+		}
+
+		if possible {
+			log.Println(gameNumber)
+			possibleGames += gameNumber
+		}
+
+	}
+
+	return possibleGames
+}
+
+// type ColorValue struct {
+// 	red   int
+// 	green int
+// 	blue  int
+// }
+
+// "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+func parseColorValues(line string) []map[string]int {
+	colorCounts := []map[string]int{}
+	gameRemovedString := strings.Split(line, ":")[1]
+	iterations := strings.Split(gameRemovedString, ";") // ["3 blue, 4 red" , "1 red, 2 green, 6 blue" , "2 green"]
+
+	for _, v := range iterations {
+		iteration := strings.Split(v, ",") // ["3 blue " , " 4 red"]
+
+		colorCountMap := map[string]int{}
+		for _, colorString := range iteration {
+			trimmed := strings.Trim(colorString, " ")
+			colorArray := strings.Split(trimmed, " ")
+			count, err := strconv.Atoi(colorArray[0])
+			color := colorArray[1]
+			if err != nil {
+				log.Panic(err)
+			}
+			colorCountMap[color] = count
+		}
+		colorCounts = append(colorCounts, colorCountMap)
+	}
+	return colorCounts
+}
+
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+func parseGameNumber(line string) int {
+	gameString := strings.Split((strings.Split(line, ":")[0]), " ")[1]
+	i, err := strconv.Atoi(string(gameString))
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return i
 }
--- day2/day2_test.go
+++ day2/day2_test.go
@@ -1,1 +1,25 @@
-package day2
+package main
+
+import (
+	"testing"
+
+	"gotest.tools/assert"
+)
+
+var input string = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
+
+func TestDay2(t *testing.T) {
+	game1 := "Game 49: 11 red, 10 green, 12 blue; 4 green, 6 red, 6 blue; 9 red, 3 blue, 7 green; 6 red, 10 green, 14 blue; 8 blue, 10 red, 5 green; 6 blue, 17 green, 6 red"
+	n1 := parseGameNumber(game1)
+
+	assert.Equal(t, n1, 49)
+
+	game2 := "Game 9: 11 red, 10 green, 12 blue; 4 green, 6 red, 6 blue; 9 red, 3 blue, 7 green; 6 red, 10 green, 14 blue; 8 blue, 10 red, 5 green; 6 blue, 17 green, 6 red"
+	n2 := parseGameNumber(game2)
+
+	assert.Equal(t, n2, 9)
+}
